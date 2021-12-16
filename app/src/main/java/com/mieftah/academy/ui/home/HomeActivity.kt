078ee496a7357d9e6fa5027f9ece22bb5b3c2a3c
Miftahul93 <miftahululum693@gmail.com>
2021-12-16 09:59:17 +0700

    menghubungkan AcademyFragment dan BookmarkFragment dengan HomeActivity

--- app/src/main/java/com/mieftah/academy/ui/home/HomeActivity.kt
+++ app/src/main/java/com/mieftah/academy/ui/home/HomeActivity.kt
@@ -3,10 +3,19 @@ package com.mieftah.academy.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mieftah.academy.R
+import com.mieftah.academy.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        val activityHomeBinding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(activityHomeBinding.root)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        activityHomeBinding.viewPager.adapter = sectionsPagerAdapter
+        activityHomeBinding.tabs.setupWithViewPager(activityHomeBinding.viewPager)
+
+        supportActionBar?.elevation = 0f
     }
 }
