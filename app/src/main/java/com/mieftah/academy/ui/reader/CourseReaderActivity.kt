30c86d24ebf1b13c5f11b333be84e3536c341a04
Miftahul93 <miftahululum693@gmail.com>
2021-12-16 21:41:38 +0700

    menghubungkan ModuleContentFragment dan ModuleListFragment dengan CourseReaderActivity

--- app/src/main/java/com/mieftah/academy/ui/reader/CourseReaderActivity.kt
+++ app/src/main/java/com/mieftah/academy/ui/reader/CourseReaderActivity.kt
@@ -3,8 +3,10 @@ package com.mieftah.academy.ui.reader
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mieftah.academy.R
+import com.mieftah.academy.ui.reader.content.ModuleContentFragment
+import com.mieftah.academy.ui.reader.list.ModuleListFragment
 
-class CourseReaderActivity : AppCompatActivity() {
+class CourseReaderActivity : AppCompatActivity(), CourseReaderCallback {
 
     companion object {
         const val EXTRA_COURSE_ID = "extra_course_id"
@@ -13,5 +15,39 @@ class CourseReaderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course_reader)
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            val courseId = bundle.getString(EXTRA_COURSE_ID)
+            if (courseId != null) {
+                populateFragment()
+            }
+        }
+    }
+
+    override fun moveTo(position: Int, moduleId: String) {
+        val fragment = ModuleContentFragment.newInstance()
+        supportFragmentManager.beginTransaction().add(R.id.frame_container, fragment, ModuleContentFragment.TAG)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount <= 1) {
+            finish()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun populateFragment() {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        var fragment = supportFragmentManager.findFragmentByTag(ModuleListFragment.TAG)
+        if (fragment == null) {
+            fragment = ModuleListFragment.newInstance()
+            fragmentTransaction.add(R.id.frame_container, fragment, ModuleListFragment.TAG)
+            fragmentTransaction.addToBackStack(null)
+        }
+        fragmentTransaction.commit()
     }
 }
