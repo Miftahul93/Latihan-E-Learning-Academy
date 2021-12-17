2c82fca9d1d2b292caca18c51f9dd528b470286a
Miftahul93 <miftahululum693@gmail.com>
2021-12-17 22:13:58 +0700

    AcademyViewModelTest

--- app/src/test/java/com/mieftah/academy/ui/academy/AcademyViewModelTest.kt
+++ app/src/test/java/com/mieftah/academy/ui/academy/AcademyViewModelTest.kt
@@ -7,5 +7,17 @@ import org.junit.Before
 
 class AcademyViewModelTest {
 
+    private lateinit var viewModel: AcademyViewModel
 
+    @Before
+    fun setUp() {
+        viewModel = AcademyViewModel()
+    }
+
+    @Test
+    fun getCourse() {
+        val courseEntities = viewModel.getCourse()
+        assertNotNull(courseEntities)
+        assertEquals(5, courseEntities.size)
+    }
 }
