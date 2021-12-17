e34066a455c97bea14c1fdc1621e60a97c61ac53
Miftahul93 <miftahululum693@gmail.com>
2021-12-17 22:22:25 +0700

    BookmarkViewModel

---
+++ app/src/test/java/com/mieftah/academy/ui/detail/DetailCourseViewModelTest.kt
@@ -0,0 +1,20 @@
+package com.mieftah.academy.ui.detail
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class DetailCourseViewModelTest {
+
+    @Test
+    fun setSelectedCourse() {
+    }
+
+    @Test
+    fun getCourse() {
+    }
+
+    @Test
+    fun getModules() {
+    }
+}
---
+++ app/src/test/java/com/mieftah/academy/ui/reader/CourseReaderViewModelTest.kt
@@ -0,0 +1,24 @@
+package com.mieftah.academy.ui.reader
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class CourseReaderViewModelTest {
+
+    @Test
+    fun setSelectedCourse() {
+    }
+
+    @Test
+    fun setSelectedModule() {
+    }
+
+    @Test
+    fun getModules() {
+    }
+
+    @Test
+    fun getSelectedModule() {
+    }
+}
